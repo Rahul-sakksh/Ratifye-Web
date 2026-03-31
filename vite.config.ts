d6a7c9a276eebba0f89f23c43f5e5a6f3c9a7b0e
SelvanSaksh <muthamizh.selvan@sakksh.com>
2026-03-31 20:06:52 +0530

    config with domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   server: {
+    host: true,
+    port: 7003,
+    allowedHosts: [
+      'ratifye.ai',
+      'www.ratifye.ai',
+      'web.ratifye.ai'
+    ]
+  }
 })
